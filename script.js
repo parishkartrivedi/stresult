5782fc73f586a7b3a8d89a5538d6ae3ad890d729
parishkartrivedi <48681110+parishkartrivedi@users.noreply.github.com>
2023-11-01 15:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,55 @@ document.addEventListener('DOMContentLoaded', function() {
     searchButton.addEventListener('click', function() {
         const rollNumber = rollNumberInput.value;
 
-        // Fetch student data from an external JSON file
-        fetch('studentData.json')
-            .then(response => response.json())
-            .then(data => {
-                if (data[rollNumber]) {
-                    const student = data[rollNumber];
-                    studentName.textContent = student.name;
-                    studentSubject.textContent = student.subject;
-                    studentMarks.textContent = student.marks;
-                    studentRemarks.textContent = student.remarks;
-                    resultContainer.style.display = 'block';
-                } else {
-                    alert('Student not found. Please check the roll number.');
-                }
-            });
+        // JSON data for six students
+        const studentData = {
+            "101": {
+                "name": "John Doe",
+                "subject": "Math",
+                "marks": 95,
+                "remarks": "Excellent"
+            },
+            "102": {
+                "name": "Jane Smith",
+                "subject": "Science",
+                "marks": 88,
+                "remarks": "Very Good"
+            },
+            "103": {
+                "name": "Alice Johnson",
+                "subject": "History",
+                "marks": 75,
+                "remarks": "Good"
+            },
+            "104": {
+                "name": "Bob Williams",
+                "subject": "English",
+                "marks": 91,
+                "remarks": "Excellent"
+            },
+            "105": {
+                "name": "Eva Davis",
+                "subject": "Physics",
+                "marks": 78,
+                "remarks": "Good"
+            },
+            "106": {
+                "name": "Chris Martin",
+                "subject": "Chemistry",
+                "marks": 85,
+                "remarks": "Very Good"
+            }
+        };
+
+        if (studentData[rollNumber]) {
+            const student = studentData[rollNumber];
+            studentName.textContent = student.name;
+            studentSubject.textContent = student.subject;
+            studentMarks.textContent = student.marks;
+            studentRemarks.textContent = student.remarks;
+            resultContainer.style.display = 'block';
+        } else {
+            alert('Student not found. Please check the roll number.');
+        }
     });
 });
